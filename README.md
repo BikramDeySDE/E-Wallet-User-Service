cec0d3ec6bd0b3d47d85e85e371894985bd47081
BIKRAM DEY <133648903+BikramDeySDE@users.noreply.github.com>
2025-12-11 11:44:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Wallet-User-Service
+E-Wallet-User-Service for E-Wallet App created by Bikram Dey
---
+++ src/test/java/com/bikram/E_Wallet/EWalletUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikram.E_Wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
